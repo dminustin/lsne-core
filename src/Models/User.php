b3e405dc70acf3a9dc5e1a8aeb8542a981b31e03
Danila Minustin <danila.minustin@gmail.com>
2023-01-19 22:30:20 +0100

    fix

--- src/Models/User.php
+++ src/Models/User.php
@@ -4,6 +4,7 @@ namespace Dminustin\LSNE\CorePackage\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Dminustin\LSNE\CorePackage\BaseClasses\LsneModuleClass;
+use Dminustin\LSNE\CorePackage\Traits\HasUUIDTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasUUIDTrait;
 
     /**
      * The attributes that are mass assignable.
---
+++ src/Traits/HasUUIDTrait.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dminustin\LSNE\CorePackage\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasUUIDTrait
+{
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Str::orderedUuid()->toString();
+            }
+        });
+    }
+}
